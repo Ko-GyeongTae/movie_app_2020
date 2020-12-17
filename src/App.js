a1131b98423234e4bd1d2743c08be9859b7865c7
Ko-Gyeongtae <kokt0203@naver.com>
2020-12-17 10:54:00 +0900

    add potato & basic of component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Potato from './potato';
 
 function App() {
-  return <div className="App"/>    
+  return (
+    <div>
+      <h1>hello</h1>
+      <Potato />
+    </div>
+  )
 }
 
 export default App;
