56aa00dd15832de8cb624d6d102df4975ffa870f
Ko-Gyeongtae <kokt0203@naver.com>
2020-12-20 01:20:13 +0900

    edit Detail screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Detail.css';
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -9,9 +10,26 @@ class Detail extends React.Component {
     }
     render() {
         const { location } = this.props;
-
+        console.log(this.props);
         if (location.state) {
-            return <span>{location.state.title}</span>;
+            let prop = location.state;
+            return (
+                <div className="movies">
+                    <div className="Movie">
+                        <img src={prop.poster} alt={prop.title} />
+                        <div className="movie__data">
+                            <h3 className="movie__title">{prop.title}</h3>
+                            <h5 className="movie__year">{prop.year}</h5>
+                            <ul className="genres">
+                                {prop.genres.map((genre, index) => (
+                                    <li key={index} className="genres__genre">{genre}</li>
+                                ))}
+                            </ul>
+                            <p className="movie__summary">{prop.summary}</p>
+                        </div>
+                    </div>
+                </div>
+            );
         } else {
             return null;
         }
